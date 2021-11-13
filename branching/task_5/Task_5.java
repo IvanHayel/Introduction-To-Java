0cddda1cf552c9d61030313383a4a88b18982483
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 01:09:10 +0300

    Task 5 from branching solved.

---
+++ branching/task_5/Task_5.java
@@ -0,0 +1,42 @@
+package branching.task_5;
+
+import static java.lang.Math.*;
+
+public class Task_5 {
+    public static void main(String[] args) {
+        Function f_x = new Function(3.1415926535);
+        System.out.println(f_x);
+
+        double result = f_x.getResult();
+        System.out.println("Result is " + result);
+    }
+}
+
+final class Function{
+    private double x;
+    private double result;
+
+    public Function(double x) {
+        this.x = x;
+        result = calculateFunctionValue();
+    }
+
+    @Override
+    public String toString() {
+        return "Function{" +
+                "x=" + x +
+                ", result=" + result +
+                '}';
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    private double calculateFunctionValue(){
+        if(x <= 3)
+            return pow(x,2)-3*x+9;
+        else
+            return 1/(pow(x,3)+6);
+    }
+}
