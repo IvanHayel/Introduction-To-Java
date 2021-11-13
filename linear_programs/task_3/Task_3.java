3cfabacb4a86ece56779a4857bf0aa27baa5472c
IvanHayel <i.hayel.dev@gmail.com>
2021-11-13 19:46:10 +0300

    Task 3 from linear programs solved.

---
+++ linear_programs/task_3/Task_3.java
@@ -0,0 +1,37 @@
+package linear_programs.task_3;
+
+import static java.lang.Math.*;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        Expression e = new Expression(3.1,4.1);
+        e.showResult();
+        System.out.println(e);
+    }
+}
+
+final class Expression {
+    private double x;
+    private double y;
+
+    public Expression(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void showResult() {
+        System.out.println("Expression value is " +
+                getExpressionValue());
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(getExpressionValue());
+    }
+
+    public double getExpressionValue() {
+        return (sin(x) + cos(y)) / (cos(x) - sin(y)) *
+                tan(x * y);
+    }
+
+}
