f3718c9b3e6434c0d0401e1ec261f3719b22665e
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 00:43:55 +0300

    Task 4 from branching solved.

---
+++ branching/task_4/Task_4.java
@@ -0,0 +1,60 @@
+package branching.task_4;
+
+import static java.lang.Math.*;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        Brick brick = new Brick(250,120,65);
+        Hole hole = new Hole(200,200);
+
+        brick.pushIntoHole(hole);
+
+    }
+}
+
+final class Brick{
+    private double x;
+    private double y;
+    private double z;
+    private double minimumFaceArea;
+
+    public Brick(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        minimumFaceArea = findMinimumFaceArea();
+    }
+
+    private double findMinimumFaceArea(){
+        return min(x*y,min(x*z,y*z));
+    }
+
+    public void pushIntoHole(Hole hole) {
+        if(isAbleToPassThroughTheHole(hole)){
+            System.out.println("The brick went through the hole.");
+        } else {
+            System.out.println("Brick can't go through the hole.");
+        }
+    }
+
+    public boolean isAbleToPassThroughTheHole(Hole hole){
+        return minimumFaceArea < hole.getArea();
+    }
+}
+
+final class Hole{
+    private double a;
+    private double b;
+    private double area;
+
+    public Hole(double a, double b) {
+        this.a = a;
+        this.b= b;
+        area = a*b;
+    }
+
+    public double getArea(){
+        return area;
+    }
+
+}
