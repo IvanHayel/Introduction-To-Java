7a92cc625a74bf2c3f35b687639566c3f4e4d4c2
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 15:29:11 +0300

    Task 3 from cycles solved.

--- cycles/task_3/Task_3.java
+++ cycles/task_3/Task_3.java
@@ -2,14 +2,14 @@ package cycles.task_3;
 
 public class Task_3 {
     public static void main(String[] args) {
-        int sum_of_hundred = getSumOfNumbersFrom(100);
+        int sum_of_hundred = getSumOfNumbersFor(100);
         System.out.print("Sum of the first hundred numbers = ");
         System.out.println(sum_of_hundred + ".");
     }
 
-    public static int getSumOfNumbersFrom(int from) {
+    public static int getSumOfNumbersFor(int number) {
         int sum = 0;
-        for (int i = 1; i <= from; i++) {
+        for (int i = 1; i <= number; i++) {
             sum += i;
         }
         return sum;
