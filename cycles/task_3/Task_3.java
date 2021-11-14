63a06cccb41571793df529598e7d99caafb60eeb
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 15:50:24 +0300

    Task 3 from cycles solved.

---
+++ cycles/task_3/Task_3.java
@@ -0,0 +1,17 @@
+package cycles.task_3;
+
+public class Task_3 {
+    public static void main(String[] args) {
+        int sum_squares_100 = getSumOfSquaresFor(100);
+        System.out.print("Sum of the squares of the first hundred numbers = ");
+        System.out.println(sum_squares_100);
+    }
+
+    public static int getSumOfSquaresFor(int number){
+        int sum = 0;
+        for(int i = 1; i < number; i++){
+            sum += i*i;
+        }
+        return sum;
+    }
+}
