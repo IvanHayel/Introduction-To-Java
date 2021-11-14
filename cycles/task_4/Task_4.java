1ea85ff2419ef719cc66094ec612b00f48c3bd31
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 16:05:20 +0300

    Task 4 from cycles solved.

---
+++ cycles/task_4/Task_4.java
@@ -0,0 +1,21 @@
+package cycles.task_4;
+
+import java.math.BigInteger;
+
+public class Task_4 {
+    public static void main(String[] args) {
+        BigInteger product_squares_200 = getProductOfSquaresFor(200);
+        System.out.print("Product of the squares of the first two hundred numbers = ");
+        System.out.println(product_squares_200);
+    }
+
+    public static BigInteger getProductOfSquaresFor(int number) {
+        BigInteger product = BigInteger.ONE;
+        BigInteger square;
+        for (int i = 1; i <= number; i++) {
+            square = BigInteger.valueOf((long) i * i);
+            product = product.multiply(square);
+        }
+        return product;
+    }
+}
