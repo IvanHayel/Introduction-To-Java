d46a62125234853f1a41de4051178259be607d53
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 14:34:13 +0300

    Task 2 from cycles solved.

---
+++ cycles/task_2/Task_2.java
@@ -0,0 +1,52 @@
+package cycles.task_2;
+
+import java.util.ArrayList;
+import java.text.DecimalFormat;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Function y = new Function(-1, 3, 0.15f);
+        System.out.println(y);
+    }
+}
+
+final class Function {
+    private float a;
+    private float b;
+    private float step;
+    ArrayList<Float> arrayList = new ArrayList<>();
+
+    public Function(float a, float b, float step) {
+        this.a = a;
+        this.b = b;
+        this.step = step;
+
+        fillArrayList();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Function values in [").append(a).append(",").append(b).append("] ");
+        result.append("with step = ").append(step).append(" : ");
+        for (Float value : arrayList) {
+            String current_value = new DecimalFormat("#0.00").format(value);
+            result.append(current_value);
+            result.append(" ");
+        }
+        return result.toString();
+    }
+
+    private void fillArrayList() {
+        float current_step = a;
+        while (current_step <= b) {
+            float current_value = getFunctionValue(current_step);
+            arrayList.add(current_value);
+            current_step += step;
+        }
+    }
+
+    private float getFunctionValue(float x) {
+        return x > 2.0 ? x : -x;
+    }
+}
