cf6ecac62e5ff09082f4d27467336a8d5f506263
IvanHayel <i.hayel.dev@gmail.com>
2021-11-14 15:56:36 +0300

    Task 3 from cycles solved.

--- cycles/task_3/Task_3.java
+++ cycles/task_3/Task_3.java
@@ -7,10 +7,10 @@ public class Task_3 {
         System.out.println(sum_squares_100);
     }
 
-    public static int getSumOfSquaresFor(int number){
+    public static int getSumOfSquaresFor(int number) {
         int sum = 0;
-        for(int i = 1; i < number; i++){
-            sum += i*i;
+        for (int i = 1; i <= number; i++) {
+            sum += i * i;
         }
         return sum;
     }
