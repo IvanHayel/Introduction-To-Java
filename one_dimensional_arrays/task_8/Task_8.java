2734a3e53e9e0dc5f785e456e0278354cdf32cda
HAYEL <i.hayel.dev@gmail.com>
2021-11-16 21:38:36 +0300

    Task 8 from one-dimensional arrays solved.

--- one_dimensional_arrays/task_8/Task_8.java
+++ one_dimensional_arrays/task_8/Task_8.java
@@ -10,30 +10,30 @@ import java.util.ArrayList;
 
 public class Task_8 {
     public static void main(String[] args) {
-        int[] sequence = {1,1,3,4,11,23,43,1,1,55,1,23,1,1,1};
-        ArrayList<Integer> numbers = getNumbers(sequence);
+        int[] sequence = {1, 1, 3, 4, 11, 23, 43, 1, 1, 55, 1, 23, 1, 1, 1};
+        ArrayList<Integer> numbers = getArrayListFilledWith(sequence);
         removeMinNumbers(numbers);
         System.out.println(numbers);
     }
 
-    public static void removeMinNumbers(ArrayList<Integer> numbers){
-        if(numbers.isEmpty()) return;
+    public static void removeMinNumbers(ArrayList<Integer> numbers) {
+        if (numbers.isEmpty()) return;
         Integer minValue = getMinValue(numbers);
-        while(numbers.contains(minValue))
+        while (numbers.contains(minValue))
             numbers.remove(minValue);
     }
 
-    public static Integer getMinValue(ArrayList<Integer> numbers){
+    public static Integer getMinValue(ArrayList<Integer> numbers) {
         Integer min = numbers.get(0);
-        for(Integer number: numbers)
-            if(min > number)
+        for (Integer number : numbers)
+            if (min > number)
                 min = number;
         return min;
     }
 
-    public static ArrayList<Integer> getNumbers(int[] sequence){
+    public static ArrayList<Integer> getArrayListFilledWith(int[] sequence) {
         ArrayList<Integer> numbers = new ArrayList<>();
-        for(int number: sequence)
+        for (int number : sequence)
             numbers.add(number);
         return numbers;
     }
