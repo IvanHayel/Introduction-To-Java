249ed1ede8432d27653321c838e9bb6a692158aa
HAYEL <i.hayel.dev@gmail.com>
2021-11-17 14:47:31 +0300

    Task 2 from one-dimensional arrays solved.

--- one_dimensional_arrays/task_2/Task_2.java
+++ one_dimensional_arrays/task_2/Task_2.java
@@ -11,32 +11,32 @@ import java.util.Arrays;
 public class Task_2 {
     public static void main(String[] args) {
         double[] numbers = {3.14, 1.59, 2.65, 3.58, 9.79, 3.23};
-        RealNumbers real_numbers = new RealNumbers(numbers);
-        System.out.println(real_numbers);
-        real_numbers.replaceLargerThan(3.14);
-        System.out.println(real_numbers);
-        real_numbers.showReplacementQuantity();
+        RealNumbers realNumbers = new RealNumbers(numbers);
+        System.out.println(realNumbers);
+        realNumbers.replaceLargerThan(3.14);
+        System.out.println(realNumbers);
+        realNumbers.showReplacementQuantity();
     }
 }
 
 final class RealNumbers {
     private double[] numbers;
-    private int replacement_quantity;
+    private int replacementQuantity;
 
     public RealNumbers(double[] numbers) {
         this.numbers = numbers;
-        replacement_quantity = 0;
+        replacementQuantity = 0;
     }
 
     public void showReplacementQuantity() {
-        System.out.println("Replacement quantity: " + replacement_quantity);
+        System.out.println("Replacement quantity: " + replacementQuantity);
     }
 
     public void replaceLargerThan(double z) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] > z) {
-                numbers[i] = z;
-                replacement_quantity++;
+        for (int counter = 0; counter < numbers.length; counter++) {
+            if (numbers[counter] > z) {
+                numbers[counter] = z;
+                replacementQuantity++;
             }
         }
     }
@@ -45,7 +45,7 @@ final class RealNumbers {
     public String toString() {
         return "RealNumbers{" +
                 "numbers=" + Arrays.toString(numbers) +
-                ", replacement quantity=" + replacement_quantity +
+                ", replacement quantity=" + replacementQuantity +
                 '}';
     }
 }
