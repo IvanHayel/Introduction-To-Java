001e69c5f1aa907842dc70ea348d373ff3f08b7e
HAYEL <i.hayel.dev@gmail.com>
2021-11-19 19:28:40 +0300

    Task 2 from matrices solved.

---
+++ matrices/task_2/Task_2.java
@@ -0,0 +1,28 @@
+package matrices.task_2;
+
+/*
+ * Дана квадратная матрица.
+ * Вывести на экран элементы, стоящие на диагонали.
+ */
+
+public class Task_2 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {50, 60, 70},
+                {33, 44, 55},
+                {51, 21, 12}
+        };
+
+        showMainDiagonalElements(matrix);
+    }
+
+    public static void showMainDiagonalElements(int[][] matrix) {
+        if (matrix.length != matrix[0].length)
+            return;
+        int matrixSize = matrix.length;
+
+        System.out.print("Elements of the main diagonal of the matrix: ");
+        for (int counter = 0; counter < matrixSize; counter++)
+            System.out.print(matrix[counter][counter]);
+    }
+}
