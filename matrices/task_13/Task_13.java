b9fabab6d288e94344c319d164378e70040d7ce8
HAYEL <i.hayel.dev@gmail.com>
2021-11-21 14:36:30 +0300

    Task 13 from matrices solved.

---
+++ matrices/task_13/Task_13.java
@@ -0,0 +1,60 @@
+package matrices.task_13;
+
+/*
+ * Отсортировать столбцы матрицы по возрастанию и убыванию значений элементов.
+ */
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Task_13 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {50, -60, 70},
+                {33, -44, 55},
+                {51, -21, 12}
+        };
+
+        System.out.println("Matrix:");
+        show(matrix);
+        matrix = transpose(matrix);
+        sortRowsAsc(matrix);
+        matrix = transpose(matrix);
+        System.out.println("Ascending order:");
+        show(matrix);
+        matrix = transpose(matrix);
+        sortRowsDesc(matrix);
+        matrix = transpose(matrix);
+        System.out.println("Descending order:");
+        show(matrix);
+    }
+
+    private static void show(int[][] matrix) {
+        for (int[] row : matrix)
+            System.out.println(Arrays.toString(row));
+    }
+
+    private static void sortRowsAsc(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++)
+            matrix[row] = Arrays.stream(matrix[row])
+                    .sorted()
+                    .toArray();
+    }
+
+    private static void sortRowsDesc(int[][] matrix) {
+        for (int row = 0; row < matrix.length; row++)
+            matrix[row] = Arrays.stream(matrix[row])
+                    .boxed()
+                    .sorted(Collections.reverseOrder())
+                    .mapToInt(Integer::intValue)
+                    .toArray();
+    }
+
+    private static int[][] transpose(int[][] matrix){
+        int[][] transposedMatrix = new int[matrix[0].length][matrix.length];
+        for(int row = 0; row < matrix.length; row++)
+            for(int column = 0; column < matrix[row].length; column++)
+                transposedMatrix[column][row] = matrix[row][column];
+        return transposedMatrix;
+    }
+}
