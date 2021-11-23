96c0f0033f00a8733bdfd8fefa619f59c4d29e71
HAYEL <i.hayel.dev@gmail.com>
2021-11-23 18:14:35 +0300

    Task 3 from sorting solved.

---
+++ sorting/task_3/Task3.java
@@ -0,0 +1,37 @@
+package sorting.task_3;
+
+/*
+ * Сортировка выбором.
+ * Дана последовательность чисел a_1 <= a_2 <= ... <= a_n.
+ * Требуется переставить элементы так, чтобы они были расположены по убыванию.
+ */
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] sequence = {5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        System.out.println("Sequence before sorting: ");
+        show(sequence);
+        selectionSort(sequence);
+        System.out.println("Sequence after sorting: ");
+        show(sequence);
+    }
+
+    private static void show(int[] array){
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void selectionSort(int[] array) {
+        for (int maximumValue = 0; maximumValue < array.length - 1; maximumValue++)
+            for (int counter = maximumValue + 1; counter < array.length; counter++)
+                if (array[maximumValue] < array[counter])
+                    swap(array, maximumValue, counter);
+    }
+
+    private static void swap(int[] array, int firstIndex, int secondIndex) {
+        int tempValue = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = tempValue;
+    }
+}
