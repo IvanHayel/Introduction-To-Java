2ba1f3371100919b841f4148cf229ebc7077dcfa
HAYEL <i.hayel.dev@gmail.com>
2021-11-27 23:57:42 +0300

    Task 9 from methods solved.

---
+++ methods/task_9/Task9.java
@@ -0,0 +1,44 @@
+package methods.task_9;
+
+/*
+ * Даны X,Y,Z,T длины сторон четырёхугольника.
+ * Написать метод(методы) вычисления его площади,
+ * если угол между сторонами длиной X и Y - прямой.
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+import static java.lang.Math.*;
+
+public class Task9 {
+    public static void main(String[] args) {
+        double[] quadrilateralLengths = new double[4];
+        fillWithRandom(quadrilateralLengths);
+        System.out.println("The lengths of the sides of the quadrilateral:");
+        show(quadrilateralLengths);
+        double quadrilateralArea = getQuadrilateralArea(quadrilateralLengths);
+        System.out.println("The area of the quadrilateral is " + quadrilateralArea);
+    }
+
+    private static void fillWithRandom(double[] array) {
+        Random generator = new Random();
+        for (int counter = 0; counter < array.length; counter++)
+            array[counter] = abs(generator.nextGaussian()) * 10 + 10;
+    }
+
+    private static void show(double[] quadrilateralLengths) {
+        System.out.println(Arrays.toString(quadrilateralLengths));
+    }
+
+    private static double getQuadrilateralArea(double... quadrilateralLengths) {
+        double perimeter = 0;
+        for (double sideLength : quadrilateralLengths)
+            perimeter += sideLength;
+        double semiPerimeter = perimeter / 2;
+        double quadrilateralArea = 1;
+        for (double sideLength : quadrilateralLengths)
+            quadrilateralArea *= semiPerimeter - sideLength;
+        return sqrt(quadrilateralArea);
+    }
+}
