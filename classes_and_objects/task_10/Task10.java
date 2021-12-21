83124ac6b423eec36ea1c876898512209e825076
HAYEL <i.hayel.dev@gmail.com>
2021-12-21 20:39:09 +0300

    Task 10 from classes and objects updated.

--- classes_and_objects/task_10/Task10.java
+++ classes_and_objects/task_10/Task10.java
@@ -3,6 +3,18 @@ package classes_and_objects.task_10;
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 
+/**
+ * Create an Airline class, the specification of which is given below.
+ * Define constructors, set- and get- methods, and the toString () method.
+ * Create a second class that aggregates the Airline array with appropriate constructors and methods.
+ * Set the criteria for selecting data and display this data on the console.
+ * Airline: destination, flight number, aircraft type, departure time, days of the week.
+ * Find and withdraw:
+ * a) a list of flights for a given destination;
+ * b) a list of flights for a given day of the week;
+ * c) a list of flights for a given day of the week, the departure time for which is greater than the given one.
+ */
+
 public class Task10 {
     public static void main(String[] args) {
         Airline unknownAirline = new Airline();
@@ -25,7 +37,7 @@ public class Task10 {
         airlines.add(secondAirline);
         airlines.add(thirdAirline);
 
-        System.out.println("Airlines with destination 'Bangkok':");
+        System.out.println("Airlines with destination - 'Bangkok':");
         airlines.printByDestination("Bangkok");
         System.out.println("Airplanes that take off on thursday:");
         airlines.printByDayOfWeek(DayOfWeek.THURSDAY);
